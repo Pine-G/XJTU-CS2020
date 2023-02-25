e0adbd59ce2f20cdb2605ed42a3a1cf0c3c3c236
Pine-G <97101322+Pine-G@users.noreply.github.com>
2023-02-25 20:35:50 +0800

    Update index_equal.cpp

--- 算法分析与设计/作业代码/index_equal.cpp
+++ 算法分析与设计/作业代码/index_equal.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 
 int index_select(int *a, int low, int high) {
-	int mid = (low + high) / 2;
-	if (low == high && low != a[low]) {
-		std::cerr << "non-existent!" << std::endl;
-		exit(0);
-	} else {
-		if (mid < a[mid])
-			return index_select(a, low, mid - 1);
-		else if (mid > a[mid])
-			return index_select(a, mid + 1, high);
-		else
-			return a[mid];
-	}
+    int mid = (low + high) / 2;
+    if (low == high && low != a[low]) {
+        std::cerr << "non-existent!" << std::endl;
+        exit(0);
+    } else {
+        if (mid < a[mid])
+            return index_select(a, low, mid - 1);
+        else if (mid > a[mid])
+            return index_select(a, mid + 1, high);
+        else
+            return a[mid];
+    }
 }
 
-//测试程序
+//娴嬭瘯绋嬪簭
 int main(void) {
-	//按升序排好的整数
-	int a[10] = { -1, 0, 1, 2, 3, 4, 5, 7, 17, 18 };
-	std::cout << index_select(a, 0, 9) << std::endl;
-	return 0;
+    //鎸夊崌搴忔帓濂界殑鏁存暟
+    int a[10] = { -1, 0, 1, 2, 3, 4, 5, 7, 17, 18 };
+    std::cout << index_select(a, 0, 9) << std::endl;
+    return 0;
 }
