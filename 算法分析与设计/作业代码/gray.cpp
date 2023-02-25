1317b95bced754cd3b633128352963ddd25142ed
Pine-G <97101322+Pine-G@users.noreply.github.com>
2023-02-25 20:34:07 +0800

    Update gray.cpp

--- 算法分析与设计/作业代码/gray.cpp
+++ 算法分析与设计/作业代码/gray.cpp
@@ -4,48 +4,48 @@ using namespace std;
 int count = 0;
 
 int *Gray(int n) {
-	int *a = new int[(int)pow(2, n) + 1];
-	if (n == 1) {
-		a[1] = 0;
-		a[2] = 1;
-		return a;
-	}
-	int *temp = Gray(n - 1);
-	for (int i = 1; i <= (int)pow(2, n - 1); i++)
-		a[i] = temp[i];
-	for (int i = (int)pow(2, n - 1) + 1, k = 0; i <= (int)pow(2, n); i++, k++)
-		a[i] = a[(int)pow(2, n - 1) - k] + (int)pow(2, n - 1);
-	delete[] temp;
-	return a;
+    int *a = new int[(int)pow(2, n) + 1];
+    if (n == 1) {
+        a[1] = 0;
+        a[2] = 1;
+        return a;
+    }
+    int *temp = Gray(n - 1);
+    for (int i = 1; i <= (int)pow(2, n - 1); i++)
+        a[i] = temp[i];
+    for (int i = (int)pow(2, n - 1) + 1, k = 0; i <= (int)pow(2, n); i++, k++)
+        a[i] = a[(int)pow(2, n - 1) - k] + (int)pow(2, n - 1);
+    delete[] temp;
+    return a;
 }
 
 void binary(int num, int n) {
-	count++;
-	int remainder;
-	if (num <= 1) {
-		if (count < n)
-			for (int i = 0; i < n - count; i++)
-				cout << 0;
-		cout << num;
-		return;
-	}
-	remainder = num % 2;
-	binary(num >> 1, n);
-	cout << remainder;
+    count++;
+    int remainder;
+    if (num <= 1) {
+        if (count < n)
+            for (int i = 0; i < n - count; i++)
+                cout << 0;
+        cout << num;
+        return;
+    }
+    remainder = num % 2;
+    binary(num >> 1, n);
+    cout << remainder;
 }
 
 void printGray(int n) {
-	int *a = Gray(n);
-	for (int i = 1; i <= (int)pow(2, n); i++) {
-		binary(a[i], n);
-		cout << endl;
-		count = 0;
-	}
+    int *a = Gray(n);
+    for (int i = 1; i <= (int)pow(2, n); i++) {
+        binary(a[i], n);
+        cout << endl;
+        count = 0;
+    }
 }
 
 int main(void) {
-	int n;
-	cin >> n;
-	printGray(n);
-	return 0;
+    int n;
+    cin >> n;
+    printGray(n);
+    return 0;
 }
