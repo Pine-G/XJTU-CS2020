5f345406c08414f8336473c35b51b36b729b870d
Pine-G <97101322+Pine-G@users.noreply.github.com>
2023-02-25 20:37:50 +0800

    Update polymerization.cpp

--- 算法分析与设计/作业代码/polymerization.cpp
+++ 算法分析与设计/作业代码/polymerization.cpp
@@ -1,101 +1,101 @@
 #include <iostream>
-#define d 6/*³£Êı*/
+#define d 6/*å¸¸æ•°*/
 
 typedef struct {
-	int *coefficient;	//¶àÏîÊ½ÏµÊı
-	int n;				//×î¸ßÏî´ÎÊı
+    int *coefficient;   //å¤šé¡¹å¼ç³»æ•°
+    int n;              //æœ€é«˜é¡¹æ¬¡æ•°
 } polynomial;
 
-int zero_point[d] = {1, 2, 3, 4, 5, 6/*Áãµã¼¯*/};
+int zero_point[d] = {1, 2, 3, 4, 5, 6/*é›¶ç‚¹é›†*/};
 
-//Çå³ı·ÖÅä¸ø¶àÏîÊ½µÄ¿Õ¼ä
+//æ¸…é™¤åˆ†é…ç»™å¤šé¡¹å¼çš„ç©ºé—´
 void clear(polynomial *p) {
-	delete[] p->coefficient;
-	delete p;
+    delete[] p->coefficient;
+    delete p;
 }
 
 polynomial *Mul1(polynomial *p1, polynomial *p2) {
-	polynomial *p = new polynomial;
-	p->n = p1->n + 1;
-	p->coefficient = new int[p->n + 1];
-	p->coefficient[0] = p1->coefficient[0] * p2->coefficient[0];
-	for (int i = 1; i <= p->n; i++) {
-		p->coefficient[i] = 0;
-		for (int j = 0; j < 2; j++) {
-			if (i - j <= p1->n && j <= i)
-				p->coefficient[i] += p1->coefficient[i - j] * p2->coefficient[j];
-		}
-	}
-	clear(p1);
-	clear(p2);
-	return p;
+    polynomial *p = new polynomial;
+    p->n = p1->n + 1;
+    p->coefficient = new int[p->n + 1];
+    p->coefficient[0] = p1->coefficient[0] * p2->coefficient[0];
+    for (int i = 1; i <= p->n; i++) {
+        p->coefficient[i] = 0;
+        for (int j = 0; j < 2; j++) {
+            if (i - j <= p1->n && j <= i)
+                p->coefficient[i] += p1->coefficient[i - j] * p2->coefficient[j];
+        }
+    }
+    clear(p1);
+    clear(p2);
+    return p;
 }
 
 polynomial *Mul2(polynomial *p1, polynomial *p2) {
-	polynomial *p = new polynomial;
-	p->n = 2 * p1->n;
-	p->coefficient = new int[p->n + 1];
-	for (int i = 0; i <= p->n; i++) {
-		p->coefficient[i] = 0;
-		for (int j = 0; j <= p1->n; j++) {
-			if (i - j <= p2->n && j <= i)
-				p->coefficient[i] += p1->coefficient[j] * p2->coefficient[i - j];
-		}
-	}
-	clear(p1);
-	clear(p2);
-	return p;
+    polynomial *p = new polynomial;
+    p->n = 2 * p1->n;
+    p->coefficient = new int[p->n + 1];
+    for (int i = 0; i <= p->n; i++) {
+        p->coefficient[i] = 0;
+        for (int j = 0; j <= p1->n; j++) {
+            if (i - j <= p2->n && j <= i)
+                p->coefficient[i] += p1->coefficient[j] * p2->coefficient[i - j];
+        }
+    }
+    clear(p1);
+    clear(p2);
+    return p;
 }
 
-//³õÊ¼»¯Ò»´Î¶àÏîÊ½P(x)=x-nd
+//åˆå§‹åŒ–ä¸€æ¬¡å¤šé¡¹å¼P(x)=x-nd
 polynomial **initialize(int *zero_point) {
-	polynomial **p = new polynomial*[d];
-	for (int i = 0; i < d; i++) {
-		p[i] = new polynomial;
-		p[i]->coefficient = new int[2];
-		p[i]->coefficient[0] = -zero_point[i];
-		p[i]->coefficient[1] = 1;
-		p[i]->n = 1;
-	}
-	return p;
+    polynomial **p = new polynomial*[d];
+    for (int i = 0; i < d; i++) {
+        p[i] = new polynomial;
+        p[i]->coefficient = new int[2];
+        p[i]->coefficient[0] = -zero_point[i];
+        p[i]->coefficient[1] = 1;
+        p[i]->n = 1;
+    }
+    return p;
 }
 
-//Çó½â¶àÏîÊ½
+//æ±‚è§£å¤šé¡¹å¼
 polynomial *poly(polynomial **p, int low, int high) {
-	//Mul1()¿ÉÒÔÔÚO(i)Ê±¼äÄÚ¼ÆËãÒ»¸öi´Î¶àÏîÊ½ÓëÒ»¸ö1´Î¶àÏîÊ½µÄ³Ë»ı
-	//Mul2()¿ÉÒÔÔÚO(ilogi)Ê±¼äÄÚ¼ÆËãÁ½¸öi´Î¶àÏîÊ½µÄ³Ë»ı
-	polynomial *ans = NULL;
-	if (low == high) {
-		ans = p[low];
-	} else if (high - low == 1) {
-		ans = Mul1(p[low], p[high]);
-	} else {
-		int mid = (low + high) / 2;
-		if ((high - low) % 2 == 0) {
-			polynomial *p1 = poly(p, low, mid - 1);
-			polynomial *p2 = poly(p, mid, high - 1);
-			polynomial *p3 = p[high];
-			ans = Mul2(p1, p2);
-			ans = Mul1(ans, p3);
-		} else {
-			polynomial *p1 = poly(p, low, mid);
-			polynomial *p2 = poly(p, mid + 1, high);
-			ans = Mul2(p1, p2);
-		}
-	}
+    //Mul1()å¯ä»¥åœ¨O(i)æ—¶é—´å†…è®¡ç®—ä¸€ä¸ªiæ¬¡å¤šé¡¹å¼ä¸ä¸€ä¸ª1æ¬¡å¤šé¡¹å¼çš„ä¹˜ç§¯
+    //Mul2()å¯ä»¥åœ¨O(ilogi)æ—¶é—´å†…è®¡ç®—ä¸¤ä¸ªiæ¬¡å¤šé¡¹å¼çš„ä¹˜ç§¯
+    polynomial *ans = NULL;
+    if (low == high) {
+        ans = p[low];
+    } else if (high - low == 1) {
+        ans = Mul1(p[low], p[high]);
+    } else {
+        int mid = (low + high) / 2;
+        if ((high - low) % 2 == 0) {
+            polynomial *p1 = poly(p, low, mid - 1);
+            polynomial *p2 = poly(p, mid, high - 1);
+            polynomial *p3 = p[high];
+            ans = Mul2(p1, p2);
+            ans = Mul1(ans, p3);
+        } else {
+            polynomial *p1 = poly(p, low, mid);
+            polynomial *p2 = poly(p, mid + 1, high);
+            ans = Mul2(p1, p2);
+        }
+    }
 
-	return ans;
+    return ans;
 }
 
 int main(void) {
-	polynomial **p = initialize(zero_point);
-	polynomial *ans = poly(p, 0, d - 1);
-	delete[] p;
+    polynomial **p = initialize(zero_point);
+    polynomial *ans = poly(p, 0, d - 1);
+    delete[] p;
 
-	for (int i = 0; i <= ans->n; i++) {
-		std::cout << ans->coefficient[i] << ' ';
-	}
-	clear(ans);
+    for (int i = 0; i <= ans->n; i++) {
+        std::cout << ans->coefficient[i] << ' ';
+    }
+    clear(ans);
 
-	return 0;
+    return 0;
 }
