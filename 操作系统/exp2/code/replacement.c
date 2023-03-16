22aaaa42c8bd14b427fb17fece6ec5886daf63b8
Pine-G <97101322+Pine-G@users.noreply.github.com>
2023-03-16 20:25:28 +0800

    Update replacement.c

--- 操作系统/exp2/code/replacement.c
+++ 操作系统/exp2/code/replacement.c
@@ -4,305 +4,305 @@
 #include <time.h>
 #include <limits.h>
 
-//Ò³±íÏî
+//é¡µè¡¨é¡¹
 typedef struct page {
-	unsigned int frame;		//Ö¡ºÅ
-	bool flag;				//±ê¼ÇÎ»
-	unsigned int counter;	//¼ÆÊıÆ÷
+    unsigned int frame;     //å¸§å·
+    bool flag;              //æ ‡è®°ä½
+    unsigned int counter;   //è®¡æ•°å™¨
 } page;
 
-page *page_table = NULL;			//Ò³±íÖ¸Õë
-unsigned int *reference = NULL;		//Ò³ÃæÒıÓÃ´®
-unsigned int table_size = 10;		//Ò³±í´óĞ¡
-unsigned int frame_size = 4;		//Ö¡±í´óĞ¡
-unsigned int ref_size = 20;			//ÒıÓÃ´®´óĞ¡
+page *page_table = NULL;            //é¡µè¡¨æŒ‡é’ˆ
+unsigned int *reference = NULL;     //é¡µé¢å¼•ç”¨ä¸²
+unsigned int table_size = 10;       //é¡µè¡¨å¤§å°
+unsigned int frame_size = 4;        //å¸§è¡¨å¤§å°
+unsigned int ref_size = 20;         //å¼•ç”¨ä¸²å¤§å°
 unsigned int belady[12] = { 1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5 };
 
-//³õÊ¼»¯Ò³±í£¬½«±ê¼ÇÎ»ÖÃÎŞĞ§£¬¼ÆÊıÆ÷ÇåÁã
+//åˆå§‹åŒ–é¡µè¡¨ï¼Œå°†æ ‡è®°ä½ç½®æ— æ•ˆï¼Œè®¡æ•°å™¨æ¸…é›¶
 void initial_page_table() {
-	page_table = (page *)malloc(table_size * sizeof(page));
-	if (!page_table) {
-		printf("page_table: malloc failed!\n");
-		exit(0);
-	}
-	for (unsigned int i = 0; i < table_size; i++) {
-		page_table[i].flag = false;
-		page_table[i].counter = 0;
-	}
+    page_table = (page *)malloc(table_size * sizeof(page));
+    if (!page_table) {
+        printf("page_table: malloc failed!\n");
+        exit(0);
+    }
+    for (unsigned int i = 0; i < table_size; i++) {
+        page_table[i].flag = false;
+        page_table[i].counter = 0;
+    }
 }
 
-//³õÊ¼»¯ÒıÓÃ´®
+//åˆå§‹åŒ–å¼•ç”¨ä¸²
 void initial_reference() {
-	reference = (unsigned int *)malloc(ref_size * sizeof(unsigned int));
-	if (!reference) {
-		printf("reference: malloc failed!\n");
-		exit(0);
-	}
-	//srand((unsigned int)time(0));		//Ëæ»úÊıÖÖ×Ó
-	for (unsigned int i = 0; i < ref_size; i++)
-		reference[i] = rand() % table_size;
+    reference = (unsigned int *)malloc(ref_size * sizeof(unsigned int));
+    if (!reference) {
+        printf("reference: malloc failed!\n");
+        exit(0);
+    }
+    //srand((unsigned int)time(0));     //éšæœºæ•°ç§å­
+    for (unsigned int i = 0; i < ref_size; i++)
+        reference[i] = rand() % table_size;
 }
 
-//Êä³öÒıÓÃ´®
+//è¾“å‡ºå¼•ç”¨ä¸²
 void print_reference() {
-	printf("\nreference:\n");
-	for (unsigned int i = 0; i < ref_size; i++) {
-		printf("%u ", reference[i]);
-	}
-	printf("\n");
+    printf("\nreference:\n");
+    for (unsigned int i = 0; i < ref_size; i++) {
+        printf("%u ", reference[i]);
+    }
+    printf("\n");
 }
 
-//ÊÍ·ÅÒ³±íºÍÒıÓÃ´®µÄÄÚ´æ
+//é‡Šæ”¾é¡µè¡¨å’Œå¼•ç”¨ä¸²çš„å†…å­˜
 void release() {
-	if (page_table)
-		free(page_table);
-	if (reference && reference != belady)
-		free(reference);
+    if (page_table)
+        free(page_table);
+    if (reference && reference != belady)
+        free(reference);
 }
 
 void Belady(void) {
-	ref_size = 12;
-	frame_size = 3;
-	table_size = 6;
-	reference = belady;
+    ref_size = 12;
+    frame_size = 3;
+    table_size = 6;
+    reference = belady;
 }
 
 int FIFO(void) {
-	unsigned int diseffect = 0;		//È±Ò³´íÎóÊı
-	unsigned int front = 0;			//¶ÓÁĞÍ·
-	unsigned int rear = 0;			//¶ÓÁĞÎ²
-	unsigned int queue_size = frame_size + 1;
-	unsigned int *queue = (unsigned int *)malloc(queue_size * sizeof(unsigned int));
-	if (!queue) {
-		printf("queue: malloc failed!\n");
-		exit(0);
-	}
+    unsigned int diseffect = 0;     //ç¼ºé¡µé”™è¯¯æ•°
+    unsigned int front = 0;         //é˜Ÿåˆ—å¤´
+    unsigned int rear = 0;          //é˜Ÿåˆ—å°¾
+    unsigned int queue_size = frame_size + 1;
+    unsigned int *queue = (unsigned int *)malloc(queue_size * sizeof(unsigned int));
+    if (!queue) {
+        printf("queue: malloc failed!\n");
+        exit(0);
+    }
 
-	for (unsigned int i = 0; i < ref_size; i++) {
-		printf("Page %u arrives ", reference[i]);
-		//Ò³Ãæ²»ÔÚÖ¡ÖĞ
-		if (!page_table[reference[i]].flag) {
-			printf("but not in the frame.\n");
-			diseffect++;
-			if ((rear + 1) % queue_size == front) {
-				//Ö¡Âú»»³ö
-				page_table[queue[front]].flag = false;
-				front = (front + 1) % queue_size;
-			}
-			//Ò³Ãæ»»Èë
-			page_table[reference[i]].flag = true;
-			page_table[reference[i]].frame = rear;
-			queue[rear] = reference[i];
-			rear = (rear + 1) % queue_size;
-			//Êä³öµ±Ç°Ö¡ÖĞÄÚÈİ
-			printf("frame: ");
-			for (unsigned int k = front; k != rear; k = (k + 1) % queue_size)
-				printf("|%u| ", queue[k]);
-			printf("\n");
-		} else {
-			//Ò³ÃæÔÚÖ¡ÖĞ
-			printf("and in the frame.\n");
-		}
-	}
+    for (unsigned int i = 0; i < ref_size; i++) {
+        printf("Page %u arrives ", reference[i]);
+        //é¡µé¢ä¸åœ¨å¸§ä¸­
+        if (!page_table[reference[i]].flag) {
+            printf("but not in the frame.\n");
+            diseffect++;
+            if ((rear + 1) % queue_size == front) {
+                //å¸§æ»¡æ¢å‡º
+                page_table[queue[front]].flag = false;
+                front = (front + 1) % queue_size;
+            }
+            //é¡µé¢æ¢å…¥
+            page_table[reference[i]].flag = true;
+            page_table[reference[i]].frame = rear;
+            queue[rear] = reference[i];
+            rear = (rear + 1) % queue_size;
+            //è¾“å‡ºå½“å‰å¸§ä¸­å†…å®¹
+            printf("frame: ");
+            for (unsigned int k = front; k != rear; k = (k + 1) % queue_size)
+                printf("|%u| ", queue[k]);
+            printf("\n");
+        } else {
+            //é¡µé¢åœ¨å¸§ä¸­
+            printf("and in the frame.\n");
+        }
+    }
 
-	free(queue);
-	return diseffect;
+    free(queue);
+    return diseffect;
 }
 
-//²éÕÒ×îÔçµ½´ïµÄÒ³Ãæ
+//æŸ¥æ‰¾æœ€æ—©åˆ°è¾¾çš„é¡µé¢
 int find_min(unsigned int *frame) {
-	unsigned int min = UINT_MAX, frame_num = 0;
-	for (unsigned int i = 0; i < frame_size; i++) {
-		if (page_table[frame[i]].counter < min) {
-			min = page_table[frame[i]].counter;
-			frame_num = i;
-		}
-	}
-	return frame_num;
+    unsigned int min = UINT_MAX, frame_num = 0;
+    for (unsigned int i = 0; i < frame_size; i++) {
+        if (page_table[frame[i]].counter < min) {
+            min = page_table[frame[i]].counter;
+            frame_num = i;
+        }
+    }
+    return frame_num;
 }
 
 unsigned int LRU(void) {
-	unsigned int diseffect = 0;		//È±Ò³´íÎóÊı
-	unsigned int clock = 0;			//Âß¼­Ê±ÖÓ
-	unsigned int full = 0;			//Ö¡Âú±êÊ¶
-	unsigned int *frame = (unsigned int *)malloc(frame_size * sizeof(unsigned int));
-	if (!frame) {
-		printf("frame: malloc failed!\n");
-		exit(0);
-	}
+    unsigned int diseffect = 0;     //ç¼ºé¡µé”™è¯¯æ•°
+    unsigned int clock = 0;         //é€»è¾‘æ—¶é’Ÿ
+    unsigned int full = 0;          //å¸§æ»¡æ ‡è¯†
+    unsigned int *frame = (unsigned int *)malloc(frame_size * sizeof(unsigned int));
+    if (!frame) {
+        printf("frame: malloc failed!\n");
+        exit(0);
+    }
 
-	for (unsigned int i = 0; i < ref_size; i++) {
-		printf("Page %u arrives ", reference[i]);
-		//¿¼ÂÇÊ±ÖÓÒç³ö
-		if (clock == UINT_MAX) {
-			unsigned int min;
-			unsigned int *temp = (unsigned int *)malloc(frame_size * sizeof(unsigned int));
-			for (unsigned int i = 0; i < frame_size; i++) {
-				min = find_min(frame);
-				temp[i] = frame[min];
-				page_table[frame[min]].counter = UINT_MAX;
-			}
-			for (unsigned int i = 0; i < frame_size; i++)
-				page_table[temp[i]].counter = i;
-			free(temp);
-			clock = frame_size;
-		}
-		page_table[reference[i]].counter = clock;
-		clock++;
-		//Ò³Ãæ²»ÔÚÖ¡ÖĞ
-		if (!page_table[reference[i]].flag) {
-			printf("but not in the frame.\n");
-			diseffect++;
-			if (full < frame_size) {
-				//Ö¡Î´ÂúÊ±
-				page_table[reference[i]].flag = true;
-				page_table[reference[i]].frame = full;
-				frame[full] = reference[i];
-				full++;
-			} else {
-				unsigned int replace = find_min(frame);
-				//Ò³Ãæ»»³ö
-				page_table[frame[replace]].flag = false;
-				//Ò³Ãæ»»Èë
-				page_table[reference[i]].flag = true;
-				page_table[reference[i]].frame = replace;
-				frame[replace] = reference[i];
-			}
-			//Êä³öµ±Ç°Ö¡ÖĞÄÚÈİ
-			printf("frame: ");
-			for (unsigned int k = 0; k < full; k++)
-				printf("|%u| ", frame[k]);
-			printf("\n");
-		} else {
-			//Ò³ÃæÔÚÖ¡ÖĞ
-			printf("and in the frame.\n");
-		}
-	}
+    for (unsigned int i = 0; i < ref_size; i++) {
+        printf("Page %u arrives ", reference[i]);
+        //è€ƒè™‘æ—¶é’Ÿæº¢å‡º
+        if (clock == UINT_MAX) {
+            unsigned int min;
+            unsigned int *temp = (unsigned int *)malloc(frame_size * sizeof(unsigned int));
+            for (unsigned int i = 0; i < frame_size; i++) {
+                min = find_min(frame);
+                temp[i] = frame[min];
+                page_table[frame[min]].counter = UINT_MAX;
+            }
+            for (unsigned int i = 0; i < frame_size; i++)
+                page_table[temp[i]].counter = i;
+            free(temp);
+            clock = frame_size;
+        }
+        page_table[reference[i]].counter = clock;
+        clock++;
+        //é¡µé¢ä¸åœ¨å¸§ä¸­
+        if (!page_table[reference[i]].flag) {
+            printf("but not in the frame.\n");
+            diseffect++;
+            if (full < frame_size) {
+                //å¸§æœªæ»¡æ—¶
+                page_table[reference[i]].flag = true;
+                page_table[reference[i]].frame = full;
+                frame[full] = reference[i];
+                full++;
+            } else {
+                unsigned int replace = find_min(frame);
+                //é¡µé¢æ¢å‡º
+                page_table[frame[replace]].flag = false;
+                //é¡µé¢æ¢å…¥
+                page_table[reference[i]].flag = true;
+                page_table[reference[i]].frame = replace;
+                frame[replace] = reference[i];
+            }
+            //è¾“å‡ºå½“å‰å¸§ä¸­å†…å®¹
+            printf("frame: ");
+            for (unsigned int k = 0; k < full; k++)
+                printf("|%u| ", frame[k]);
+            printf("\n");
+        } else {
+            //é¡µé¢åœ¨å¸§ä¸­
+            printf("and in the frame.\n");
+        }
+    }
 
-	free(frame);
-	return diseffect;
+    free(frame);
+    return diseffect;
 }
 
-//ÉèÖÃ²ÎÊı
+//è®¾ç½®å‚æ•°
 char set_parameter(void) {
-	//ÖÃ»»Ëã·¨»òBeladyÒì³£ÏÖÏó
-	printf("\nChoice: 1)FIFO      2)LRU      3)FIFO(Belady)\n");
-	char choice = getchar();
-	while (getchar() != '\n');
-	if (choice == '3') {
-		Belady();
-		return choice;
-	}
+    //ç½®æ¢ç®—æ³•æˆ–Beladyå¼‚å¸¸ç°è±¡
+    printf("\nChoice: 1)FIFO      2)LRU      3)FIFO(Belady)\n");
+    char choice = getchar();
+    while (getchar() != '\n');
+    if (choice == '3') {
+        Belady();
+        return choice;
+    }
 
-	unsigned int size = 0;
-	//Ò³±í´óĞ¡
-	printf("Please enter the size(0 < size <= 128) of the page_table: ");
-	if (scanf("%u", &size) == 1 && size > 0 && size <= 128)
-		table_size = size;
-	else {
-		table_size = 10;
-		printf("default = 10\n");
-	}
-	while (getchar() != '\n');
-	//Ö¡±í´óĞ¡
-	printf("Please enter the size(0 < size <= 32) of the frame: ");
-	if (scanf("%u", &size) == 1 && size > 0 && size <= 32)
-		frame_size = size;
-	else {
-		frame_size = 4;
-		printf("default = 4\n");
-	}
-	while (getchar() != '\n');
-	//ÒıÓÃ´®´óĞ¡
-	printf("Please enter the size(0 <= size <= 100) of the reference: ");
-	if (scanf("%u", &size) == 1 && size >= 0 && size <= 100)
-		ref_size = size;
-	else {
-		ref_size = 20;
-		printf("default = 20\n");
-	}
-	while (getchar() != '\n');
+    unsigned int size = 0;
+    //é¡µè¡¨å¤§å°
+    printf("Please enter the size(0 < size <= 128) of the page_table: ");
+    if (scanf("%u", &size) == 1 && size > 0 && size <= 128)
+        table_size = size;
+    else {
+        table_size = 10;
+        printf("default = 10\n");
+    }
+    while (getchar() != '\n');
+    //å¸§è¡¨å¤§å°
+    printf("Please enter the size(0 < size <= 32) of the frame: ");
+    if (scanf("%u", &size) == 1 && size > 0 && size <= 32)
+        frame_size = size;
+    else {
+        frame_size = 4;
+        printf("default = 4\n");
+    }
+    while (getchar() != '\n');
+    //å¼•ç”¨ä¸²å¤§å°
+    printf("Please enter the size(0 <= size <= 100) of the reference: ");
+    if (scanf("%u", &size) == 1 && size >= 0 && size <= 100)
+        ref_size = size;
+    else {
+        ref_size = 20;
+        printf("default = 20\n");
+    }
+    while (getchar() != '\n');
 
-	return choice;
+    return choice;
 }
 
 int main(void) {
-	printf("*********************************\n");
-	printf("       PAGE REPLACEMENT\n");
-	printf("*********************************\n\n");
+    printf("*********************************\n");
+    printf("       PAGE REPLACEMENT\n");
+    printf("*********************************\n\n");
 
-	char choice1 = set_parameter();
-	if (choice1 != '3')
-		initial_reference();
-	unsigned int diseffect;
+    char choice1 = set_parameter();
+    if (choice1 != '3')
+        initial_reference();
+    unsigned int diseffect;
 
-	while (true) {
-		print_reference();
-		initial_page_table();
+    while (true) {
+        print_reference();
+        initial_page_table();
 
-		switch (choice1) {
-			case '1':
-				printf("\nFIFO:\n\n");
-				diseffect = FIFO();
-				break;
-			case '2':
-				printf("\nLRU:\n\n");
-				diseffect = LRU();
-				break;
-			case '3':
-				printf("\nFIFO(Belady):\n\n");
-				diseffect = FIFO();
-				break;
-			default:
-				printf("\ndefault = FIFO:\n\n");
-				diseffect = FIFO();
-		}
+        switch (choice1) {
+            case '1':
+                printf("\nFIFO:\n\n");
+                diseffect = FIFO();
+                break;
+            case '2':
+                printf("\nLRU:\n\n");
+                diseffect = LRU();
+                break;
+            case '3':
+                printf("\nFIFO(Belady):\n\n");
+                diseffect = FIFO();
+                break;
+            default:
+                printf("\ndefault = FIFO:\n\n");
+                diseffect = FIFO();
+        }
 
-		printf("\nPage fault: %d\n", diseffect);
-		printf("ratio: %.2f%%\n", (1.0 - (float)diseffect / ref_size) * 100.0);
+        printf("\nPage fault: %d\n", diseffect);
+        printf("ratio: %.2f%%\n", (1.0 - (float)diseffect / ref_size) * 100.0);
 
-		printf("\nQuit? [defualt[Enter] = 'No', [y] = 'yes']: ");
-		//ÍË³ö³ÌĞò
-		if (getchar() == 'y') {
-			release();
-			break;
-		}
+        printf("\nQuit? [defualt[Enter] = 'No', [y] = 'yes']: ");
+        //é€€å‡ºç¨‹åº
+        if (getchar() == 'y') {
+            release();
+            break;
+        }
 
-		printf("\nReset? [defualt[Enter] = 'No', [y] = 'yes']: ");
-		if (getchar() == 'y') {
-			while (getchar() != '\n');
-			//ÖØÖÃ²ÎÊı
-			release();
-			choice1 = set_parameter();
-			if (choice1 != '3')
-				initial_reference();
-		} else {
-			printf("\n1)Change the algorithm       2)Change the size of frame\n");
-			unsigned int size = 0;
-			char choice2 = getchar();
-			while (getchar() != '\n');
-			switch (choice2) {
-				case '1':
-					//¸ü¸ÄËã·¨
-					choice1 = (choice1 == '2') ? '1' : '2';
-					break;
-				case '2':
-					//¸ü¸ÄÖ¡±í´óĞ¡
-					printf("\nThe new size(0 < size <= 32) of the frame: ");
-					if (scanf("%u", &size) == 1 && size > 0 && size <= 32)
-						frame_size = size;
-					else {
-						frame_size = 4;
-						printf("default = 4\n");
-					}
-					while (getchar() != '\n');
-					break;
-				default:
-					printf("default: Change the algorithm\n");
-					choice1 = (choice1 == '2') ? '1' : '2';
-			}
-		}
-	}
+        printf("\nReset? [defualt[Enter] = 'No', [y] = 'yes']: ");
+        if (getchar() == 'y') {
+            while (getchar() != '\n');
+            //é‡ç½®å‚æ•°
+            release();
+            choice1 = set_parameter();
+            if (choice1 != '3')
+                initial_reference();
+        } else {
+            printf("\n1)Change the algorithm       2)Change the size of frame\n");
+            unsigned int size = 0;
+            char choice2 = getchar();
+            while (getchar() != '\n');
+            switch (choice2) {
+                case '1':
+                    //æ›´æ”¹ç®—æ³•
+                    choice1 = (choice1 == '2') ? '1' : '2';
+                    break;
+                case '2':
+                    //æ›´æ”¹å¸§è¡¨å¤§å°
+                    printf("\nThe new size(0 < size <= 32) of the frame: ");
+                    if (scanf("%u", &size) == 1 && size > 0 && size <= 32)
+                        frame_size = size;
+                    else {
+                        frame_size = 4;
+                        printf("default = 4\n");
+                    }
+                    while (getchar() != '\n');
+                    break;
+                default:
+                    printf("default: Change the algorithm\n");
+                    choice1 = (choice1 == '2') ? '1' : '2';
+            }
+        }
+    }
 
-	return 0;
+    return 0;
 }
