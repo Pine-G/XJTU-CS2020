00641ebec6a69b6450b4073e9826391f60c294d8
Pine-G <97101322+Pine-G@users.noreply.github.com>
2023-03-16 20:16:53 +0800

    Update pid.c

--- 操作系统/exp1/code/pid.c
+++ 操作系统/exp1/code/pid.c
@@ -4,30 +4,30 @@
 #include <wait.h>
 
 int main() {
-	pid_t pid, pid1;
+    pid_t pid, pid1;
 
-	// fork a child process
-	pid = fork();
+    // fork a child process
+    pid = fork();
 
-	// error occurred
-	if (pid < 0) {
-		fprintf(stderr, "Fork Failed\n");
-		return 1;
-	}
-	// child process
-	else if (pid == 0) {
-		pid1 = getpid();
-		printf("child: pid = %d\n", pid);
-		printf("child: pid1 = %d\n", pid1);
-	}
-	// parent process
-	else {
-		//wait(NULL);
-		pid1 = getpid();
-		printf("parent: pid = %d\n", pid);
-		printf("parent: pid1 = %d\n", pid1);
-		wait(NULL);
-	}
+    // error occurred
+    if (pid < 0) {
+        fprintf(stderr, "Fork Failed\n");
+        return 1;
+    }
+    // child process
+    else if (pid == 0) {
+        pid1 = getpid();
+        printf("child: pid = %d\n", pid);
+        printf("child: pid1 = %d\n", pid1);
+    }
+    // parent process
+    else {
+        //wait(NULL);
+        pid1 = getpid();
+        printf("parent: pid = %d\n", pid);
+        printf("parent: pid1 = %d\n", pid1);
+        wait(NULL);
+    }
 
-	return 0;
+    return 0;
 }
