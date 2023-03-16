dcbc2938faabd4bf4abb4d91daf0e935063826bf
Pine-G <97101322+Pine-G@users.noreply.github.com>
2023-03-16 20:17:29 +0800

    Update system.c

--- 操作系统/exp1/code/system.c
+++ 操作系统/exp1/code/system.c
@@ -5,25 +5,25 @@
 #include <stdlib.h>
 
 int main() {
-	// fork a child process
-	pid_t pid = fork();
+    // fork a child process
+    pid_t pid = fork();
 
-	// error occurred
-	if (pid < 0) {
-		fprintf(stderr, "Fork Failed\n");
-		return 1;
-	}
-	// child process
-	else if (pid == 0) {
-		printf("child: pid = %d\n", getpid());
-		//system("./hello");
-		execl("./hello", "hello", NULL);
-	}
-	// parent process
-	else {
-		wait(NULL);
-		printf("parent: pid = %d\n", getpid());
-	}
+    // error occurred
+    if (pid < 0) {
+        fprintf(stderr, "Fork Failed\n");
+        return 1;
+    }
+    // child process
+    else if (pid == 0) {
+        printf("child: pid = %d\n", getpid());
+        //system("./hello");
+        execl("./hello", "hello", NULL);
+    }
+    // parent process
+    else {
+        wait(NULL);
+        printf("parent: pid = %d\n", getpid());
+    }
 
-	return 0;
+    return 0;
 }
