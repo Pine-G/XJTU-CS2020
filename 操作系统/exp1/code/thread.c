06c5cf3b85dbbe50ba1076f7f4d926b46483cb39
Pine-G <97101322+Pine-G@users.noreply.github.com>
2023-03-16 20:18:02 +0800

    Update thread.c

--- 操作系统/exp1/code/thread.c
+++ 操作系统/exp1/code/thread.c
@@ -7,51 +7,51 @@ void *add(void *param);
 void *sub(void *param);
 
 int main() {
-	int info;
-	pthread_t tid1, tid2;
+    int info;
+    pthread_t tid1, tid2;
 
-	// create the mutex lock
-	pthread_mutex_init(&mutex, NULL);
+    // create the mutex lock
+    pthread_mutex_init(&mutex, NULL);
 
-	// create the thread
-	info = pthread_create(&tid1, NULL, add, NULL);
-	if (info != 0) {
-		printf("Thread1 create failed!\n");
-		return -1;
-	}
-	info = pthread_create(&tid2, NULL, sub, NULL);
-	if (info != 0) {
-		printf("Thread2 create failed!\n");
-		return -1;
-	}
+    // create the thread
+    info = pthread_create(&tid1, NULL, add, NULL);
+    if (info != 0) {
+        printf("Thread1 create failed!\n");
+        return -1;
+    }
+    info = pthread_create(&tid2, NULL, sub, NULL);
+    if (info != 0) {
+        printf("Thread2 create failed!\n");
+        return -1;
+    }
 
-	// wait for the thread to exit
-	pthread_join(tid1, NULL);
-	pthread_join(tid2, NULL);
+    // wait for the thread to exit
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
 
-	printf("count = %d\n", count);
+    printf("count = %d\n", count);
 
-	return 0;
+    return 0;
 }
 
 void *add(void *param) {
-	pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex);
 
-	// critical section
-	for (int i = 0; i < 5000; i++)
-		count++;
-	printf("thread1: count = %d\n", count);
+    // critical section
+    for (int i = 0; i < 5000; i++)
+        count++;
+    printf("thread1: count = %d\n", count);
 
-	pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex);
 }
 
 void *sub(void *param) {
-	pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex);
 
-	// critical section
-	for (int i = 0; i < 5000; i++)
-		count--;
-	printf("thread2: count = %d\n", count);
+    // critical section
+    for (int i = 0; i < 5000; i++)
+        count--;
+    printf("thread2: count = %d\n", count);
 
-	pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex);
 }
