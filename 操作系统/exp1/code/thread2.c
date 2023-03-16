e8d899946306ea69a7e6504c451491c9cc807f36
Pine-G <97101322+Pine-G@users.noreply.github.com>
2023-03-16 20:18:54 +0800

    Update thread2.c

--- 操作系统/exp1/code/thread2.c
+++ 操作系统/exp1/code/thread2.c
@@ -8,28 +8,26 @@
 void *runner(void *param);
 
 int main() {
-	int info;
-	pthread_t tid;
+    int info;
+    pthread_t tid;
 
-	printf("Parent: syscall(SYS_gettid) = %ld, getpid() = %d, pthread_self() = %lu\n", syscall(SYS_gettid), getpid(),
-	       pthread_self());
+    printf("Parent: syscall(SYS_gettid) = %ld, getpid() = %d, pthread_self() = %lu\n", syscall(SYS_gettid), getpid(), pthread_self());
 
-	// create the thread
-	info = pthread_create(&tid, NULL, runner, NULL);
-	if (info != 0) {
-		printf("Thread create failed!\n");
-		return -1;
-	}
+    // create the thread
+    info = pthread_create(&tid, NULL, runner, NULL);
+    if (info != 0) {
+        printf("Thread create failed!\n");
+        return -1;
+    }
 
-	// wait for the thread to exit
-	pthread_join(tid, NULL);
+    // wait for the thread to exit
+    pthread_join(tid, NULL);
 
-	return 0;
+    return 0;
 }
 
 void *runner(void *param) {
-	printf("Child: syscall(SYS_gettid) = %ld, getpid() = %d, pthread_self() = %lu\n", syscall(SYS_gettid), getpid(),
-	       pthread_self());
-	//system("./hello2");
-	execl("./hello2", "hello2", NULL);
+    printf("Child: syscall(SYS_gettid) = %ld, getpid() = %d, pthread_self() = %lu\n", syscall(SYS_gettid), getpid(), pthread_self());
+    //system("./hello2");
+    execl("./hello2", "hello2", NULL);
 }
